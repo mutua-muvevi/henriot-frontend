c5b45060e5e5e19863f3e9426dacefba1d6fcc10
SamsAgency <samstore637@gmail.com>
2022-07-28 14:05:15 +0300

    completed dynamic addition of disclosure context

--- src/pages/auth/setup/disclosuresform.js
+++ src/pages/auth/setup/disclosuresform.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Alert, AlertTitle, Box, Button, Grid, Grow } from "@mui/material";
+import { Alert, AlertTitle, Box, Button, ButtonGroup, Grid, Grow } from "@mui/material";
 import { styled } from "@mui/system";
 
 import { FieldArray } from "formik"
@@ -14,33 +14,56 @@ const StyledDisclosureForm = styled(Box)(({ theme }) => ({
 	
 }));
 
-const StyledDisclosureFormContentGrid = styled(Grid)(({ theme }) => ({
-
+const StyledDisclosureFormContent = styled(Box)(({ theme }) => ({
+	display: "flex",
+	flexDirection: "column"
 }));
 
-const StyledDisclosureFormContentGridItem = styled(Grid)(({ theme }) => ({
+const StyledDisclosureFormContentGridCheckBox = styled(Grid)(({ theme }) => ({
+	margin: "10px auto"
+}));
+
+const StyledDisclosureFormContentGridItemCheckBox = styled(Grid)(({ theme }) => ({
+	// backgroundColor: "green",
+	
+}));
+
+const StyledDisclosureFormContentGridFieldArrayWrapper = styled(Box)(({ theme }) => ({
+	margin: "10px auto",
+}));
+
+const StyledDisclosureFormContentGridFieldArray = styled(Grid)(({ theme }) => ({
+	margin: "10px auto"
+}));
+
+const StyledDisclosureFormContentGridItemFieldArray = styled(Grid)(({ theme }) => ({
+	
 
 }));
 
 const DisclosureForm = ({values}) => {
 	return (
 		<StyledDisclosureForm>
-			<StyledDisclosureFormContentGrid container spacing={2}>
-				{
-					setupfields &&
-					setupfields.disclosures &&
-					setupfields.disclosures.map((el, i) => (
-						<StyledDisclosureFormContentGridItem key={i} item xl={el.xl} lg={el.lg} md={el.md} sm={el.sm} xs={el.xs}>
-							<CheckBoxField
-								name={el.name}
-								label={el.label}
-								type={el.type}
-								placeholder={el.placeholder}
-								size="small"
-							/>
-						</StyledDisclosureFormContentGridItem>
-					))
-				}
+			<StyledDisclosureFormContent>
+
+				<StyledDisclosureFormContentGridCheckBox container>
+					{
+						setupfields &&
+						setupfields.disclosures &&
+						setupfields.disclosures.map((el, i) => (
+							<StyledDisclosureFormContentGridItemCheckBox key={i} item xl={el.xl} lg={el.lg} md={el.md} sm={el.sm} xs={el.xs}>
+								<CheckBoxField
+									name={el.name}
+									label={el.label}
+									type={el.type}
+									placeholder={el.placeholder}
+									size="small"
+								/>
+							</StyledDisclosureFormContentGridItemCheckBox>
+						))
+					}
+				</StyledDisclosureFormContentGridCheckBox>
+
 				<FieldArray 
 					name="disclosures.context"
 					render={
@@ -53,58 +76,59 @@ const DisclosureForm = ({values}) => {
 										context &&
 										context.length > 0 ? 
 										context.map((el, index) => (
-											<Box key={index}>
-												{
-													setupfields &&
-													setupfields.disclosuresContext &&
-													setupfields.disclosuresContext.map((el, i) => (
-														<StyledDisclosureFormContentGridItem key={i} item xl={el.xl} lg={el.lg} md={el.md} sm={el.sm} xs={el.xs}>
-															<TextfieldWrapper
-																name={`disclosures.context.${index}.${el.name}`}
-																label={el.label}
-																type={el.type}
-																placeholder={el.placeholder}
-																size="small"
-															/>
-														</StyledDisclosureFormContentGridItem>
-													))
-												}
-												<Button
-													type="button"
-													onClick={() => arrayHelpers.remove(index)}
-													variant="contained"
-													color="error"
+											<StyledDisclosureFormContentGridFieldArrayWrapper>
+												<StyledDisclosureFormContentGridFieldArray container rowSpacing={3} key={index}>
+													{
+														setupfields &&
+														setupfields.disclosuresContext &&
+														setupfields.disclosuresContext.map((el, i) => (
+															<StyledDisclosureFormContentGridItemFieldArray key={i} item xl={el.xl} lg={el.lg} md={el.md} sm={el.sm} xs={el.xs}>
+																<TextfieldWrapper
+																	name={`disclosures.context.${index}.${el.name}`}
+																	label={el.label}
+																	type={el.type}
+																	placeholder={el.placeholder}
+																	size="small"
+																/>
+															</StyledDisclosureFormContentGridItemFieldArray>
+														))
+													}
+												</StyledDisclosureFormContentGridFieldArray>
+												<ButtonGroup type="button" variant="contained">
+													<Button
+														color="error"
+														onClick={() => arrayHelpers.remove(index)}
+														>
+														Remove context
+													</Button>
+													
+													<Button
+														color="primary"
+														onClick = {
+															() => arrayHelpers.push({
+																context_type: "",
+																company_name: "",
+																company_street_address: "",
+																company_state: "",
+																company_city: "",
+																company_country: "",
+																subheader: "",
+																company_compliance_email: "",
+															})
+														}
 													>
-													Remove context
-												</Button>
-											</Box>
+														Add another context
+													</Button>
+												</ButtonGroup>
+											</StyledDisclosureFormContentGridFieldArrayWrapper>
 										)) : null
 									}
-									<Button
-										type="button"
-										variant="outlined"
-										color="primary"
-										onClick = {
-											() => arrayHelpers.push({
-												context_type: "",
-												company_name: "",
-												company_street_address: "",
-												company_state: "",
-												company_city: "",
-												company_country: "",
-												subheader: "",
-												company_compliance_email: "",
-											})
-										}
-									>
-										Add context
-									</Button>
 								</>
 							)
 						}
 					}
 				/>
-			</StyledDisclosureFormContentGrid>
+			</StyledDisclosureFormContent>
 		</StyledDisclosureForm>
 	)
 }
