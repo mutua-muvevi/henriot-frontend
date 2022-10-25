ebc491c409a86bc9e8a220a62c14c51e6164f45f
SamsAgency <samstore637@gmail.com>
2022-10-25 11:50:04 +0300

    added tweets section under research > market cards

--- src/pages/app/wealth/research/market/market.js
+++ src/pages/app/wealth/research/market/market.js
@@ -12,7 +12,7 @@ const StyledResearchMarketItems = styled(Grid)(({ theme }) => ({
 
 const ResearchMarket = () => {
 	return (
-		<StyledResearchMarket container spacing={1}>
+		<StyledResearchMarket container spacing={2}>
 			{
 				[1,2,3,4,5,6,7,8].map((_, el) => (
 					<StyledResearchMarketItems key={el} item xs={12} sm={6} md={4} lg={3} xl={3}>
